3dbad40ba54c21ae310f9e7faec10b080eec8a7a
Daniel <danielboone1990@gmail.com>
2021-11-22 21:06:25 +0700

    redirect user if active session, restyle homepage

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,3 +1,4 @@
+from django.http.response import JsonResponse
 from rest_framework import generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -76,3 +77,14 @@ class JoinRoom(APIView):
                 return Response({'Message': 'Room Joined!'}, status=status.HTTP_200_OK)
             return Response({'Bad Request': 'Invalid room code, room does not exist'}, status=status.HTTP_404_NOT_FOUND)
         return Response({'Bad Request': 'Invalid post data, did not find a code key'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserInRoom(APIView):
+    def get(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        data = {
+            'code': self.request.session.get('room_code')
+        }
+        return JsonResponse(data, status=status.HTTP_200_OK)
